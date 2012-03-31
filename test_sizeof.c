cad3fa2a5e17f0c7dbfa3a2bf199d0de29cee2f8
masa <masa@c406673e-950c-47ff-9500-ef9fbfbf93bd>
2012-03-31 05:23:47 +0000

    add sizeof testcase
    git-svn-id: svn+ssh://www.ubicg.ynu.ac.jp/svn/konoha-project/konoha2/trunk@252 c406673e-950c-47ff-9500-ef9fbfbf93bd

--- test_sizeof.c
+++ test_sizeof.c
@@ -5,6 +5,13 @@ int main(int argc, const char *argv[])
     assert(sizeof(kObject) <= 64);
     assert(sizeof(knh_Fmethod) == sizeof(void*));
     assert(sizeof(float) <= sizeof(void*));
+
+    assert(sizeof(kint_t) == sizeof(kfloat_t));
+    assert(sizeof(kint_t) == sizeof(void*));
+    assert(sizeof(ksfp_t) == sizeof(krbp_t) * 2);
+    assert(sizeof(krbp_t) == sizeof(void*));
+    assert(sizeof(intptr_t) == sizeof(kint_t));
+    assert(sizeof(kshort_t) * 2 == sizeof(intptr_t));
     fprintf(stderr, "%ld\n", sizeof(kObject));
     return 0;
 }
