f135585da1064535ce54413891926839aaf1687b
masa <masa@c406673e-950c-47ff-9500-ef9fbfbf93bd>
2012-04-03 01:19:17 +0000

    refactor testcases and remove io package warnings
    git-svn-id: svn+ssh://www.ubicg.ynu.ac.jp/svn/konoha-project/konoha2/trunk@285 c406673e-950c-47ff-9500-ef9fbfbf93bd

---
+++ test_cpp.cpp
@@ -0,0 +1,10 @@
+#include "konoha2/konoha2.h"
+#include "konoha2/klib.h"
+#include "konoha2/sugar.h"
+#include "konoha2/float.h"
+#include "konoha2/gc.h"
+/* checking header files can compile at g++ */
+int main(int argc, char const* argv[])
+{
+    return 0;
+}
