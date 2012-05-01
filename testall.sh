680b6d08500bc7633babb7cdb234b52d3c5e9b4c
kimio <kimio@c406673e-950c-47ff-9500-ef9fbfbf93bd>
2012-05-01 20:30:19 +0000

    konoha2 sync test in Release
    git-svn-id: svn+ssh://www.ubicg.ynu.ac.jp/svn/konoha-project/konoha2/trunk@455 c406673e-950c-47ff-9500-ef9fbfbf93bd

--- testall.sh
+++ testall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find ./test -maxdepth 2 -name '*.k' | while read utest; do
+find ./test -maxdepth 3 -name '*.k' | while read utest; do
 	#echo "testing $utest .."
 	konoha2 --test-with $utest || echo "[FAILED] $utest"
 done
