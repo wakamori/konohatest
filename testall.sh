39c0f0f47c951aaf3724a137dd35c34b8364519a
kimio <kimio@c406673e-950c-47ff-9500-ef9fbfbf93bd>
2012-05-01 12:49:39 +0000

    konoha2 moved kimio's MINI to Release
    git-svn-id: svn+ssh://www.ubicg.ynu.ac.jp/svn/konoha-project/konoha2/trunk@442 c406673e-950c-47ff-9500-ef9fbfbf93bd

--- testall.sh
+++ testall.sh
@@ -2,6 +2,6 @@
 
 find ./test -maxdepth 2 -name '*.k' | while read utest; do
 	#echo "testing $utest .."
-	konoha2 --test-with $utest || echo "[FAILED] <SEGV> $utest"
+	konoha2 --test-with $utest || echo "[FAILED] $utest"
 done
 
