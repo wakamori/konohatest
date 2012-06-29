e593059162643c87a4c82bf4ae890f198804560b
wakamori <wakamori111@gmail.com>
2012-06-29 14:37:03 +0900

    remove ckonoha dependent files

--- test-multiline.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-exec $1 < $2
--- testall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-find ./test -maxdepth 3 -name '*.k' | while read utest; do
-	#echo "testing $utest .."
-	konoha2 --test-with $utest || echo "[FAILED] $utest"
-done
-
